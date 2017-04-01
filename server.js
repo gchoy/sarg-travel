810f1e9636b63c5a5bb2a649c00346bc9ee5b6c4
gchoy <gabriela.choy@gmail.com>
2017-04-01 08:51:45 -0700

    Added create function to postController.js and added endpoint to server.js

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/templates/:name', function templates(req, res) {
+  var name = req.params.name;
+  res.sendFile(__dirname + '/views/templates/' + name + '.html');
+});
+
 var controllers = require('./controllers');
+
+
 
 app.get('/api', controllers.api.index);
 app.get('/api/posts', controllers.posts.index);
-
-
+app.get('/api/posts/:postId', controllers.posts.show);
+app.post('/api/posts', controllers.posts.create);
 
 
 
