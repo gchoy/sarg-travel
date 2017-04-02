5041188c5b7052c2018b322607e4f5e8a1f5ea0d
emamiali <emamiali1993@gmail.com>
2017-04-01 17:54:02 -0700

    added the user authentication files and folders

--- models/user.js
+++ models/user.js
@@ -3,12 +3,46 @@ var mongoose = require('mongoose'),
     bcrypt = require('bcryptjs')
 
 var UserSchema = new Schema({
-  name: String,
-  CurrentCity: String,
-  DateJoined: {type: Date, default: Date.now()},
-  image: String
+    name: String,
+    CurrentCity: String,
+    DateJoined: {
+        type: Date,
+        default: Date.now()
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        select: false
+    },
+    image: String
 });
 
-var User = mongoose.model('User', UserSchema);
+// bump date updated
+UserSchema.pre('save', function(next) {
+    this.updated = Date.now();
 
+    // encrypt password
+    var user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        done(err, isMatch);
+    });
+}; // this.password refers to UserSchema password
+
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
