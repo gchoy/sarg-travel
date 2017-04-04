2819e088a7fa819e579b2e3c1fdbbf58201fac82
aapiane09 <aapiane09@gmail.com>
2017-04-03 23:30:04 -0700

    Fixed issues in routes, added index view

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -4,10 +4,12 @@ function configRoutes($routeProvider, $locationProvider) {
 
 $routeProvider
   .when('/', {
-  templateUrl: 'views/index.html'
+    // Should point to index view (public/templates/index.html), not your layout (views/index.html)
+  templateUrl: 'templates/index.html'
   })
   .when('/register', {
-  templateUrl: 'public/templates/user/register.html',
+    // Your default directory is already set to public, so public/templates... is not necessary
+  templateUrl: 'templates/user/register.html',
   controller: 'SignupController',
   controllerAs: 'sc',
   resolve: {
